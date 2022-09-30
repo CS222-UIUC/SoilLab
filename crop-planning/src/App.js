a2226479ce6ce06b8d7024a45ab5815849ebb686
Lily Zhang <1640220783@qq.com>
2022-09-30 17:01:06 -0500

    create a draft for the main UI page, add sidebar and corresponding pages for each component of the side bar

--- crop-planning/src/App.js
+++ crop-planning/src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
 import React from 'react';
+import Sidebar from './components/sidebar';
+import AccountDashboard from './pages/accountDashboard';
+import CropModelBoard from './pages/cropModelBoard';
+import SuggestionBoard from './pages/suggestionBoard';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Crop Planning</h1>
+        <script src="https://kit.fontawesome.com/1bee948766.js" crossOrigin="anonymous"></script>
+
       </header>
+      <BrowserRouter>
+        <Sidebar>
+          <Routes>
+            <Route path='/accountDashboard' element={<AccountDashboard/>}/>
+            <Route path='/cropModelBoard' element={<CropModelBoard/>}/>
+            <Route path='/suggestionBoard' element={<SuggestionBoard/>}/>
+          </Routes>
+        </Sidebar>
+      </BrowserRouter>
     </div>
   );
 }
