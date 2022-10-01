8cba3e43b9240a72b8bfe8d4cf8ae83a00d94329
anashen <anaghas2@illinois.edu>
2022-09-30 21:57:21 -0500

    Set up Firebase auth - register & login

--- crop-planning/src/App.js
+++ crop-planning/src/App.js
@@ -1,28 +1,22 @@
-import './App.css';
-import React from 'react';
-import Sidebar from './components/sidebar';
-import AccountDashboard from './pages/accountDashboard';
-import CropModelBoard from './pages/cropModelBoard';
-import SuggestionBoard from './pages/suggestionBoard';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import "./App.css";
+import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from "./Login";
+import Register from "./Register";
+import Reset from "./Reset";
+import Dashboard from "./Dashboard";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Crop Planning</h1>
-        <script src="https://kit.fontawesome.com/1bee948766.js" crossOrigin="anonymous"></script>
-
-      </header>
-      <BrowserRouter>
-        <Sidebar>
-          <Routes>
-            <Route path='/accountDashboard' element={<AccountDashboard/>}/>
-            <Route path='/cropModelBoard' element={<CropModelBoard/>}/>
-            <Route path='/suggestionBoard' element={<SuggestionBoard/>}/>
-          </Routes>
-        </Sidebar>
-      </BrowserRouter>
+    <div className="app">
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Login />} />
+          <Route exact path="/register" element={<Register />} />
+          <Route exact path="/reset" element={<Reset />} />
+          <Route exact path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
