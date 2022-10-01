7594e108019426e9f1bfadc7ecf9c5dc40017fee
anashen <anaghas2@illinois.edu>
2022-09-30 22:17:24 -0500

    Add & pass UTs

--- crop-planning/src/App.js
+++ crop-planning/src/App.js
@@ -9,6 +9,10 @@ import Dashboard from "./Dashboard";
 function App() {
   return (
     <div className="app">
+      <header className="App-Header">
+        <h1>SoilLab</h1>
+        <script src="https://kit.fontawesome.com/1bee948766.js" crossOrigin="anonymous"></script>
+      </header>
       <Router>
         <Routes>
           <Route exact path="/" element={<Login />} />
--- crop-planning/src/__tests__/App.test.js
+++ crop-planning/src/__tests__/App.test.js
@@ -1,12 +1,21 @@
-import { render, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
+import {render, screen} from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import React from 'react'
+import '@testing-library/jest-dom'
 import App from '../App';
+import {BrowserRouter, MemoryRouter} from 'react-router-dom'
 
-test('renders header', () => {
+// test('renders header', () => {
+//   render(<App/>);
+
+//   // await userEvent.click(screen.getByText('Load Greeting'))
+
+//   const headerElement = screen.getByText('Crop Planning');
+//   expect(headerElement).toBeInTheDocument();
+// });
+
+test('Page loads properly & shows correct landing screen', () => {
   render(<App/>);
-
-  // await userEvent.click(screen.getByText('Load Greeting'))
-
-  const headerElement = screen.getByText('Crop Planning');
+  const headerElement = screen.getByText('SoilLab');
   expect(headerElement).toBeInTheDocument();
 });
