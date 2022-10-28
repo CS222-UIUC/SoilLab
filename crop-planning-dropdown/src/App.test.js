be468c45a793843351234198965678189f03fdbd
Trisha Choudhary <trishac2@illinois.edu>
2022-10-28 16:52:17 -0500

    Added tests for checking if form renders properly

--- crop-planning-dropdown/src/App.test.js
+++ crop-planning-dropdown/src/App.test.js
@@ -6,10 +6,8 @@ import React from 'react';
 test('Renders entire form (all fields + submit button)', async() => {
   render(<App />);
 
-  expect(screen.getByText('Feedback Form')).toBeInTheDocument();
-  expect(screen.getByText('First Name')).toBeInTheDocument();
-  expect(screen.getByText('Last Name')).toBeInTheDocument();
-  expect(screen.getByText('Email Address')).toBeInTheDocument();
-  expect(screen.getByText('Feedback')).toBeInTheDocument();
+  expect(screen.getByText('Before you start planning...')).toBeInTheDocument();
+  expect(screen.getByText('Location')).toBeInTheDocument();
+  expect(screen.getByText('Temperature Range')).toBeInTheDocument();
   expect(screen.getByText('Submit')).toBeInTheDocument();
 });
