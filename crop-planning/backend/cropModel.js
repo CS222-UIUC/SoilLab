7a8abe554ca4ef72edf381e796d1e43f2bc9560c
Santhosh Mohan <santhoshm309@gmail.com>
2022-11-17 10:29:23 -0600

    Lily's changes

---
+++ crop-planning/backend/cropModel.js
@@ -0,0 +1,56 @@
+/*
+
+attributes: a dictionary.
+
+what an dictionary input should look like:
+{
+    LowerTemperatureLimit: (float, unit in Farenheit), higherTemperatureLimit: (float, unit in Farenheit),
+    LowerIrrigationLimit: (float, unit in cm), HigherIrrigationLimit: (float, unit in cm), (these are the depth of water per week)
+    LowerSunLightHour: (float, unit in hour), 
+    Radius(unit in cm, radius of the area needed for growth): (float)
+    (can add more)
+}
+
+a sample attributes dictionary input:
+
+
+{
+    LowerTemperatureLimit: 59.0, higherTemperatureLimit: 77.0,
+    LowerIrrigationLimit: 20.0, HigherIrrigationLimit: 100.0,
+    LowerSunLightHour: 1500.0,
+    Radius(unit in cm, radius of the area needed for growth): 20.0
+    ** the area needed for the plant is a square, the side length of the square is 2 * radius + 1
+    (can add more)
+}
+
+*/
+
+import { Crop } from "./crop";
+
+class CropAttrs {
+    constructor (attributes) {
+        this.lowerTemperatureLimit = attributes.LowerTemperatureLimit;
+        this.higherTemperatureLimit = attributes.higherTemperatureLimit;
+        this.lowerIrrigationLimit = attributes.LowerIrrigationLimit;
+        this.higherIrrigationLimit = attributes.HigherIrrigationLimit;
+        this.lowerSunLightHour = attributes.LowerSunLightHour; 
+        this.radius = attributes.Radius;
+    }
+}
+
+class CropModel {
+    name = "Crop";
+    description = "N/A";
+
+    constructor (name, description, attributes, bn) {
+        this.name = name;
+        this.description = description;
+        if (attributes instanceof CropAttrs) {
+            this.attributes = attributes;
+        } else {
+            this.attributes = new CropAttrs(attributes);
+        }
+    }
+}
+
+export {CropModel, CropAttrs};
---
+++ crop-planning/backend/cropModelLibrary.js
@@ -0,0 +1,50 @@
+import {CropModel, CropAttrs} from './cropModel';
+
+/*
+
+References:
+https://www.almanac.com/plant/
+
+*/
+
+let CornAttributes = {
+    LowerTemperatureLimit: 60.0, higherTemperatureLimit: 95.0,
+    LowerIrrigationLimit: 2.0, HigherIrrigationLimit: 4.0,
+    LowerSunLightHour: 6.0,
+    Radius: 4.0
+}
+
+let TomatoAttributes = {
+    LowerTemperatureLimit: 55.0, higherTemperatureLimit: 77.0,
+    LowerIrrigationLimit: 2.0, HigherIrrigationLimit: 4.0,
+    LowerSunLightHour: 8.0,
+    Radius: 40.0
+}
+
+let CarrotAttributes = {
+    LowerTemperatureLimit: 45.0, higherTemperatureLimit: 75.0,
+    LowerIrrigationLimit: 2.5, HigherIrrigationLimit: 4.0,
+    LowerSunLightHour: 6.0,
+    Radius: 4.0
+}
+
+let LettuceAttributes = {
+    LowerTemperatureLimit: 45.0, higherTemperatureLimit: 70.0,
+    LowerIrrigationLimit: 1.5, HigherIrrigationLimit: 2.2,
+    LowerSunLightHour: 6.0,
+    Radius: 13
+}
+
+const CornModel = new CropModel("Corn", "Wow", CornAttributes, );
+const TomatoModel = new CropModel("Tomato", "Wow", TomatoAttributes);
+const CarrotModel = new CropModel("Carrot", "Wow", CarrotAttributes);
+const LettuceModel = new CropModel("Lettuce", "Wow", LettuceAttributes);
+
+const CropModels = {
+    Corn: CornModel,
+    Tomato: TomatoModel,
+    Carrot: CarrotModel,
+    Lettuce: LettuceModel
+}
+
+export {CropModels};
