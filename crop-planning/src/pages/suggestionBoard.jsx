0fbfe0ff4548bad4ba94a1cc6b4da7cd4e3a90e3
anashen <anaghas2@illinois.edu>
2022-12-06 01:56:29 -0600

    Add elements & text to suggestion board page for demo

--- crop-planning/src/pages/suggestionBoard.jsx
+++ crop-planning/src/pages/suggestionBoard.jsx
@@ -1,11 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
+import "./Board/suggestionBoard.css"
+import { Link } from "react-router-dom";
 
 const SuggestionBoard = () => {
+    const [showAnalysis, setShowAnalysis] = useState(false);
+    // const [showButton, setShowButton] = useState(false);
+    // const showAlert = () => {
+    //   alert("I'm an alert");
+    // }
+  
     return (
-        <div>
-            <h1>this is suggestion board yahhhh</h1>
-        </div>
+      <>
+      {!showAnalysis? 
+      <div className="text">
+      Analyze your grid layout!
+      </div> : null}
+      
+      {showAnalysis? <div className="text__analysis">
+        <b><i>SoilLab analyzed your grid layout: </i></b><br/>
+        <br/>
+        <b><font color="#3C8586">Carrot at (1,2)</font></b> and <b><font color="#3C8586">Corn at (2,3)</font></b> do not follow the recommended crop distance. <br/>
+        <br/>
+        For the selected season and environment, <b><font color="#3C8586">Rice</font></b> is not recommended.
+        <br/>
+        <br/>
+        <br/>
+        <div className="shadow">Don't agree with our suggestions? Give feedback <Link to="/form">here</Link>.</div>
+      </div> : null}
+
+      {!showAnalysis? <button className="btn" onClick={() => setShowAnalysis(!showAnalysis)}>Show Analysis</button> : null}
+      {showAnalysis? <button className="btn" onClick={() => setShowAnalysis(!showAnalysis)}>Hide Analysis</button> : null}
+      
+      </>
     );
-};  
+  }
 
 export default SuggestionBoard;
